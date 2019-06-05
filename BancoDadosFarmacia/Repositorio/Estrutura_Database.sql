cd1f05799f60a9fab9d052e9aeca7007f418ec99
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-05 15:44:15 -0300

    Desenvolvimento - Criação do Banco de dados e da Classe
    Alteração no nome da Estrutura do Banco de Dados para mais representar o banco atual.

---
+++ BancoDadosFarmacia/Repositorio/Estrutura_Database.sql
@@ -0,0 +1,23 @@
+﻿-- Tabela Comestiveis
+DROP TABLE comestiveis;
+CREATE TABLE comestiveis (
+	
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(100)NOT NULL,
+	preco DECIMAL(8,2),
+	data_vencimento DATETIME2,
+	quantidade INT,
+	marca VARCHAR(100)
+);
+
+SELECT * FROM comestiveis
+
+
+-- Tabela Produtos Higiene
+CREATE TABLE protudos_higiene (
+		id INT PRIMARY KEY IDENTITY (1,1),
+		nome VARCHAR(100),
+		categoria VARCHAR(100),
+		preco DECIMAL(8,2)
+
+);
