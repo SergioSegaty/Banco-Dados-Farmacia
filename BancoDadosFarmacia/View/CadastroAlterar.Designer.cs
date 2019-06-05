648ccc00bf98c9b1d0bf7c089243a635aa98f2ac
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-05 14:47:29 -0300

    Desenvolvido no forms a chamada do AlterarCadastro

---
+++ BancoDadosFarmacia/View/CadastroAlterar.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace View
+{
+    partial class CadastroAlterar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.dtpDataVencimento = new System.Windows.Forms.DateTimePicker();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblDataVencimento = new System.Windows.Forms.Label();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Location = new System.Drawing.Point(149, 193);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 28);
+            this.btnSalvar.TabIndex = 23;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // dtpDataVencimento
+            // 
+            this.dtpDataVencimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataVencimento.Location = new System.Drawing.Point(118, 159);
+            this.dtpDataVencimento.Name = "dtpDataVencimento";
+            this.dtpDataVencimento.Size = new System.Drawing.Size(139, 20);
+            this.dtpDataVencimento.TabIndex = 22;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(35, 94);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(139, 20);
+            this.txtMarca.TabIndex = 16;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(198, 94);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(139, 20);
+            this.txtQuantidade.TabIndex = 20;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(198, 55);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(139, 20);
+            this.txtPreco.TabIndex = 18;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(35, 52);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(139, 20);
+            this.txtNome.TabIndex = 14;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(324, 12);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(13, 13);
+            this.lblID.TabIndex = 25;
+            this.lblID.Text = "0";
+            // 
+            // lblDataVencimento
+            // 
+            this.lblDataVencimento.AutoSize = true;
+            this.lblDataVencimento.Location = new System.Drawing.Point(135, 143);
+            this.lblDataVencimento.Name = "lblDataVencimento";
+            this.lblDataVencimento.Size = new System.Drawing.Size(104, 13);
+            this.lblDataVencimento.TabIndex = 21;
+            this.lblDataVencimento.Text = "Data de Vencimento";
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(195, 78);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(62, 13);
+            this.lblQuantidade.TabIndex = 19;
+            this.lblQuantidade.Text = "Quantidade";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(32, 77);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 15;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(195, 39);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(35, 13);
+            this.lblPreco.TabIndex = 17;
+            this.lblPreco.Text = "Preço";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(283, 12);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 24;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(32, 36);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 13;
+            this.lblNome.Text = "Nome";
+            // 
+            // CadastroAlterar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 257);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dtpDataVencimento);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblDataVencimento);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblNome);
+            this.Name = "CadastroAlterar";
+            this.Text = "CadastroAlterar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.DateTimePicker dtpDataVencimento;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblDataVencimento;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
---
+++ BancoDadosFarmacia/View/CadastroAlterar.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+using Repositorio;
+
+namespace View
+{
+    public partial class CadastroAlterar : Form
+    {
+        public CadastroAlterar()
+        {
+            InitializeComponent();
+        }
+
+        public CadastroAlterar(Comestivel comesestivel)
+        {
+            InitializeComponent();
+
+            txtNome.Text = comesestivel.Nome;
+            txtMarca.Text = comesestivel.Marca;
+            txtPreco.Text = Convert.ToString(comesestivel.Preco);
+            txtQuantidade.Text = comesestivel.Quantidade.ToString();
+            lblID.Text = comesestivel.Id.ToString();
+            dtpDataVencimento.Value = comesestivel.DataVencimento;
+        }
+
+        private void SalvarRegistro()
+        {
+            Comestivel comestivel = new Comestivel();
+
+            comestivel.Id = Convert.ToInt32(lblID.Text);
+
+            comestivel.Nome = txtNome.Text;
+            if (comestivel.Nome.Length < 3)
+            {
+                MessageBox.Show("Por favor, registre o nome do produto.");
+                txtNome.Focus();
+                return;
+            }
+
+            comestivel.Marca = txtMarca.Text;
+            if (comestivel.Marca.Length < 3)
+            {
+                MessageBox.Show("Por favor, registre a marca do produto.");
+                txtMarca.Focus();
+                return;
+            }
+
+
+            try
+            {
+                comestivel.Preco = Convert.ToDecimal(txtPreco.Text);
+                if (comestivel.Preco < 0)
+                {
+                    MessageBox.Show("O valor não pode ser menor que 0");
+                    txtPreco.Focus();
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Somente números");
+                txtPreco.Focus();
+                return;
+            }
+
+            try
+            {
+                comestivel.Quantidade = Convert.ToInt32(txtQuantidade.Text);
+                if (comestivel.Quantidade < 0)
+                {
+                    MessageBox.Show("Somente números maiores que 0");
+                    txtQuantidade.Focus();
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Somente números!");
+                txtQuantidade.Focus();
+                return;
+            }
+
+            comestivel.DataVencimento = dtpDataVencimento.Value;
+            DateTime hoje = DateTime.Now;
+            if (comestivel.DataVencimento <= hoje)
+            {
+                MessageBox.Show("Por favor selecione a data de validade");
+                dtpDataVencimento.Select();
+                SendKeys.Send("%{DOWN}");
+                return;
+            }
+
+            RepositorioComestiveis repositorio = new RepositorioComestiveis();
+            repositorio.AtualizarRegistro(comestivel);
+            Close();
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            SalvarRegistro();
+        }
+    }
+}
