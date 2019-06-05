c082912335741431b45ad8be38ac9eea2bd6155a
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-05 14:48:46 -0300

    Tratamento do Cadastro (ifs e try-catches)

--- BancoDadosFarmacia/View/CadastroComestiveis.cs
+++ BancoDadosFarmacia/View/CadastroComestiveis.cs
@@ -29,20 +29,74 @@ namespace View
             Comestivel comestivel = new Comestivel();
 
             comestivel.Nome = txtNome.Text;
+            if (comestivel.Nome.Length < 3)
+            {
+                MessageBox.Show("Por favor, registre o nome do produto.");
+                txtNome.Focus();
+                return;
+            }
 
             comestivel.Marca = txtMarca.Text;
+            if (comestivel.Marca.Length < 3)
+            {
+                MessageBox.Show("Por favor, registre a marca do produto.");
+                txtMarca.Focus();
+                return;
+            }
 
-            comestivel.Preco = Convert.ToDecimal(txtPreco.Text);
 
-            comestivel.Quantidade = Convert.ToInt32(txtQuantidade.Text);
+            try
+            {
+                comestivel.Preco = Convert.ToDecimal(txtPreco.Text);
+                if (comestivel.Preco < 0)
+                {
+                    MessageBox.Show("O valor não pode ser menor que 0");
+                    txtPreco.Focus();
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Somente números");
+                txtPreco.Focus();
+                return;
+            }
 
-            comestivel.dataVencimento = dtpDataVencimento.Value;
+            try
+            {
+                comestivel.Quantidade = Convert.ToInt32(txtQuantidade.Text);
+                if (comestivel.Quantidade < 0)
+                {
+                    MessageBox.Show("Somente números maiores que 0");
+                    txtQuantidade.Focus();
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Somente números!");
+                txtQuantidade.Focus();
+                return;
+            }
+
+            comestivel.DataVencimento = dtpDataVencimento.Value;
+            DateTime hoje = DateTime.Now;
+            if (comestivel.DataVencimento <= hoje)
+            {
+                MessageBox.Show("Por favor selecione a data de validade");
+                dtpDataVencimento.Select();
+                SendKeys.Send("%{DOWN}");
+                return;
+            }
 
             RepositorioComestiveis repositorio = new RepositorioComestiveis();
             repositorio.Inserir(comestivel);
-            
 
-            
+        }
+
+        private void DtpDataVencimento_DropDown(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
