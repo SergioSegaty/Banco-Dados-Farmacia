747bc7e2914ac0caeab5257cee71c16477428b18
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-06 14:21:32 -0300

    Criação da Classe Remedios

---
+++ BancoDadosFarmacia/Model/Remedios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public class Remedios
+    {
+        public int ID;
+        public string Nome;
+        public string Categoria;
+        public string ContraIndicacoes;
+        public string Faixa;
+        public bool Generico;
+        public bool Solido;
+        public bool Receita;
+        public decimal Preco;
+
+    }
+}
