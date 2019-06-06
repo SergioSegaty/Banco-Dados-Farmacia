e90560df95461ca29a656968f637a097a5dabfab
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-06 14:21:55 -0300

    Criação da Estrutura no DB

--- BancoDadosFarmacia/Repositorio/Estrutura_Database.sql
+++ BancoDadosFarmacia/Repositorio/Estrutura_Database.sql
@@ -23,3 +23,17 @@ CREATE TABLE produtos_higiene (
 
 );
 SELECT * FROM produtos_higiene;
+
+-- Tabela Remedios
+
+CREATE TABLE produtos_remedios(
+		id INT PRIMARY KEY IDENTITY (1,1),
+		nome VARCHAR(100),
+		categoria VARCHAR(100),
+		faixa VARCHAR(100),
+		contra_indicacao TEXT,
+		preco DECIMAL(10,2),
+		generico BIT,
+		solido BIT,
+		receita BIT
+);
